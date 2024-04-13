646ac5f4f3cf4260540b297cccf28aa8de8fe085
kauatorresfranca <kauatorresfranca2@gmail.com>
2024-04-13 12:35:32 -0300

    commit inicial

---
+++ src/exercicio_ts.ts
@@ -0,0 +1,9 @@
+/*function multiplicar(valor1: number, valor2:number){
+    return valor1 * valor2
+}
+*/
+const multiplicar = (valor1:number, valor2:number) => valor1 * valor2
+const saudacoes = (nome: string) => `Olá, ${nome} !`
+
+console.log(`O resultado da multiplicação é de ${multiplicar(5,6)}`)
+console.log(saudacoes("kauã"))
